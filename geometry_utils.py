e541a9e9c262c5623680d52b5f2bc0085caa0b2c
Thomas Bittner <thomasbittner@hotmail.de>
2026-02-17 08:57:57 +0100

    trajectories drawing

--- geometry_utils.py
+++ geometry_utils.py
@@ -6,45 +6,44 @@ class TrajectoryManager:
     def __init__(self, max_points=30, max_lost_frames=5):
         self.max_points = max_points
         self.max_lost_frames = max_lost_frames
-        self.tracks = defaultdict(lambda: deque(maxlen=self.max_points))
-        # Tracks how many consecutive frames an ID has been missing
-        self.lost_counters = defaultdict(int)
+        self.tracks = {}
 
-    def update(self, obj_id, pos):
-        """Called when YOLO sees the object."""
-        self.tracks[obj_id].append(pos)
-        self.lost_counters[obj_id] = 0  # Reset counter because it's seen
+    def update(self, obj_id, pos, cropped_box=None):
+        if obj_id not in self.tracks:
+            self.tracks[obj_id] = {
+                "points": deque(maxlen=self.max_points),
+                "crops": deque(maxlen=self.max_points),
+                "lost_count": 0
+            }
+
+        track = self.tracks[obj_id]
+        track["points"].append(pos)
+        track["crops"].append(cropped_box)
+        track["lost_count"] = 0
 
     def process_garbage_collection(self, active_ids):
-        """
-        Increments 'lost' counters for missing IDs and deletes them if
-        they exceed max_lost_frames.
-        """
+        # Use list() because we are deleting keys while iterating
         all_stored_ids = list(self.tracks.keys())
 
         for obj_id in all_stored_ids:
+            track = self.tracks[obj_id]
+
             if obj_id not in active_ids:
-                self.lost_counters[obj_id] += 1
+                track["lost_count"] += 1
             else:
-                self.lost_counters[obj_id] = 0
+                track["lost_count"] = 0
 
-            # Delete if gone for too long
-            if self.lost_counters[obj_id] > self.max_lost_frames:
+            if track["lost_count"] > self.max_lost_frames:
                 del self.tracks[obj_id]
-                del self.lost_counters[obj_id]
-                car_got_lost()
-
 
     def draw(self, frame):
-        """
-        Strictly draws. No deletion logic here.
-        """
-        for obj_id, points in self.tracks.items():
+        for obj_id, data in self.tracks.items():
+            points = data["points"]
             if len(points) < 2:
                 continue
 
-            # Draw lines between points
             for i in range(1, len(points)):
-                # Visual logic: thickness grows as points get 'fresher'
                 thickness = int(2 * (i / self.max_points) + 1)
                 cv2.line(frame, points[i - 1], points[i], (0, 255, 255), thickness)
+
+
