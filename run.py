3702450cb39179131dbb43b605d3a13a9daaaa9b
Thomas Bittner <thomasbittner@hotmail.de>
2026-03-08 13:29:24 +0700

    opening windows

--- run.py
+++ run.py
@@ -4,7 +4,10 @@ import cv2
 
 def run():
     model = YOLO("yolo11m.pt")
-    video_paths = [r"AICity22_Track1_MTMC_Tracking\test\S06\c041/vdo.avi", r"AICity22_Track1_MTMC_Tracking\test\S06\c042/vdo.avi"]
+    video_paths = [
+        r"AICity22_Track1_MTMC_Tracking\test\S06\c041/vdo.avi",
+        r"AICity22_Track1_MTMC_Tracking\test\S06\c042/vdo.avi",
+    ]
     caps = [cv2.VideoCapture(video_path) for video_path in video_paths]
 
     while True:
@@ -16,7 +19,8 @@ def run():
             break
 
         for i, frame in enumerate(frames):
-            cv2.imshow(f"Video {i}", frame)
+            half_frame = cv2.resize(frame, None, fx=0.5, fy=0.5)
+            cv2.imshow(f"Video {i}", half_frame)
 
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
